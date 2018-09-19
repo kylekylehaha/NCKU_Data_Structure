cd94849bf7ed2394587aa44543b0907267105bb7
kyle <kyle@AppledeMacBook-Pro.local>
2018-09-19 20:31:58 +0800

    1.0

---
+++ HW1/hw1.1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAX_SIZE 1000
+
+int i , icount ;
+float number[MAX_SIZE];
+
+void check(){
+	int ocount = 0;
+	int index = 0;
+	int output[icount];
+
+	/*check whether is float or not*/
+	for(i=0;i<icount;i++){
+		float origin = number[i];
+		int x = (int)number[i];
+		float y = (float)x;
+		if((origin - y) == 0){
+			ocount ++;
+			output[index] = (int)y;
+			index++;
+		}
+	}
+
+	/*printf*/
+	printf("%d\n",ocount);
+	for(i=0;i<ocount;i++){
+		printf("%d\n",output[i]);
+	}
+
+	return ; 	
+}
+
+int main(){
+
+	scanf("%d",&icount);
+	
+	for(i=0;i<icount;i++){
+		scanf("%f",&number[i]);
+	}
+
+	check();
+	
+	return 0; 
+}
